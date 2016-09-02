1b59a31a813479a6e644820354c06837a98914ec
Prabhu <prabhunathan.g@gmail.com>
2016-09-01 21:25:17 -0500

    new

---
+++ app/src/main/java/com/prabhunathan/cs478/hw1sep7/MainActivity.java
@@ -0,0 +1,16 @@
+package com.prabhunathan.cs478.hw1sep7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Prabhunathan Gnanasekaran was here");
+        setContentView(R.layout.activity_main);
+    }
+}
